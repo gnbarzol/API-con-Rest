3ddae9f155a034277c9443b6c92ad54d67af0357
Gary Barzola <gnbarzol@espol.edu.ec>
2020-05-25 17:56:30 -0500

    Implementacion de joi para validacion de ingreso de datos a traves de schemas, ademas agrego los middleware en los entre los endpoints y la deficion de las rutas

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
-//Para mantener el contro sobre que aplicacion va a consumir nuestra ruta
+const {
+    movieIdSchema,
+    createMovieSchema,
+    updateMovieSchema
+  } = require('../utils/schemas/movies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
+//Para mantener el control sobre que aplicacion va a consumir nuestra ruta
 //La única responsabilidad de las rutas es saber como recibe parametros y como se los envia a los servicios para que estos los resuelvan y nos envien los datos que necesitamos
 function moviesApi(app) {
     const router = express.Router();
@@ -26,7 +34,7 @@ function moviesApi(app) {
         }
     });
 
-    router.get('/:movieId', async function(req, res, next) {
+    router.get('/:movieId', validationHandler({ movieId: movieIdSchema}, 'params'), async function(req, res, next) {
         const { movieId } = req.params;
         try{
             const movie = await moviesServices.getMovie({ movieId });
@@ -39,7 +47,7 @@ function moviesApi(app) {
         }
     });
 
-    router.post('/', async function(req, res, next) {
+    router.post('/', validationHandler(createMovieSchema), async function(req, res, next) {
         const { body: movie } = req; //Body tendra un alias llamado movie con body: movie
         try{
             const createMovieId = await moviesServices.createMovie({ movie });
@@ -52,7 +60,7 @@ function moviesApi(app) {
         }
     });
 
-    router.put('/:movieId', async function(req, res, next) {
+    router.put('/:movieId', validationHandler({movieId: movieIdSchema}, 'params'), validationHandler(updateMovieSchema), async function(req, res, next) {
         const { body: movie } = req;
         const { movieId } = req.params;
         try{
@@ -80,7 +88,7 @@ function moviesApi(app) {
     //     }
     // });
 
-    router.delete('/:moviedId', async function(req, res, next) {
+    router.delete('/:moviedId', validationHandler({movieId: movieIdSchema}), async function(req, res, next) {
         const { moviedId } = req.params;
         try{
             const deletedMovieId = await moviesServices.deleteMovie({ moviedId });
